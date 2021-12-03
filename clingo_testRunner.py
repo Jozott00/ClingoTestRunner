4092cae2ee198d4f1ebb3075e14b28ce7373eec1
Johannes Zottele <official@johannes-zottele.at>
2021-12-03 13:32:23 +0100

    Improve argument parsing

--- clingo_testRunner.py
+++ clingo_testRunner.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, argparse
 from subprocess import PIPE, STDOUT, CalledProcessError, TimeoutExpired, check_output
 
 m_anno = "%mnr:"
@@ -10,22 +10,17 @@ failed_tests = 0
 succeeded_tests = 0
 
 
-def usage():
-    print("Usage: %s FILE COUNT" % sys.argv[0])
-    exit(1)
-
-
 def error_exit(reason):
-    print("[%s] Error: %s" % (sys.argv[0], reason))
+    print("❌ [%s] Error: %s" % (sys.argv[0], reason))
     exit(1)
 
 
 # Parse MNR annotation
-def parse_mnr(line):
+def parse_mnr(line, file):
     try:
         return int(line.split(":")[1])
     except ValueError:
-        error_exit('wrong usage of "%% mnr: N" annotation in: %s' % line)
+        error_exit(f"wrong usage of '%mnr: N' annotation in {file} in line: {line}")
 
 
 # Parse INCS annotation
@@ -34,7 +29,6 @@ def parse_incs(line):
     return line.split(";")
 
 
-# TODO: integrate set_of_sets -- so check if all items of all sets are included in at least one answerset
 def run_test(path, model_size, set_of_sets):
     cmd = ["clingo", "-n", "0", path, file_name]
 
@@ -43,11 +37,13 @@ def run_test(path, model_size, set_of_sets):
     except CalledProcessError as e:
         # clingo exists with 30 as retun code
         if e.returncode != 30:
-            error_exit("'%s' failed with the reason above" % (" ".join(e.cmd)))
+            return print(f"❗ Error: '{' '.join(e.cmd)}' failed")
         else:
             output = e.output
     except TimeoutExpired as e:
-        error_exit("'%s' failed because timeout of 2s was expired" % " ".join(e.cmd))
+        return print(
+            f"❗ Error: '{' '.join(e.cmd)}' failed because timeout of 2s was expired"
+        )
 
     output = output.decode("utf-8").split("\n")
 
@@ -64,7 +60,7 @@ def run_test(path, model_size, set_of_sets):
                     found_sets[i] = True
         line = "".join(line.split())
         if "Models:" in line:
-            actual_model_size = parse_mnr(line)
+            actual_model_size = parse_mnr(line, path)
         if "Answer:" in line:
             answer_next_line = True
 
@@ -107,7 +103,7 @@ def test_for_file(path):
         with open(path) as f:
             lines = f.read().split("\n")
     except FileNotFoundError:
-        return print("❗ Error: %s does not exist" % path)
+        return print(f"❗ Error: {path} does not exist")
 
     model_size = 0
     set_with_facts = []
@@ -116,11 +112,11 @@ def test_for_file(path):
         # remove whitespaces
         line = "".join(line.split())
         if m_anno in line:
-            model_size = parse_mnr(line)
+            model_size = parse_mnr(line, path)
         elif s_anno in line:
             set_with_facts.append(parse_incs(line))
 
-    if model_size is 0:
+    if model_size == 0:
         print("❗ WARNING -- No model size given. This leads to inaccurate tests!")
     else:
         print(f"💡 INFO -- Exactly {model_size} models must be found")
@@ -138,20 +134,31 @@ def test_for_file(path):
 
 
 def main():
-    if len(sys.argv) != 3:
-        usage()
+    parser = argparse.ArgumentParser(description="Automate clingo tests.")
+    parser.add_argument(
+        "file", metavar="FILE", type=str, nargs=1, help=".dl file to test"
+    )
+    parser.add_argument(
+        "size",
+        metavar="SIZE",
+        type=int,
+        nargs=1,
+        help="Number of test files. Must be of format <file_to_test>_<x>.dl, must be in same folder as the file to test and <x> must begin with 0",
+    )
+
+    args = parser.parse_args()
 
     global file_name
-    file_name = sys.argv[1]
+    file_name = args.file[0]
 
     if not os.path.isfile(file_name):
-        error_exit("File to test does not exist")
+        error_exit(f"File {file_name} does not exist")
 
-    test_file_count = int(sys.argv[2])
+    test_file_count = int(args.size[0])
     test_prefix = file_name.split(".")[0]
 
-    for i in range(0, test_file_count):
-        test_for_file("%s_%d.dl" % (test_prefix, i))
+    for i in range(test_file_count):
+        test_for_file(f"{test_prefix}_{i}.dl")
 
     print(
         f"""
@@ -165,7 +172,7 @@ RESULTS:"""
     elif succeeded_tests > 0:
         print(f"✅ PASSED: {succeeded_tests}")
 
-    if failed_tests is not 0:
+    if failed_tests != 0:
         print(f"❌ FAILED: {failed_tests}")
     if failed_tests + succeeded_tests is not test_file_count:
         print(
